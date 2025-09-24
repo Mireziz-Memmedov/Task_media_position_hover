1b256476b1cc5ce5907bc62b8eec4b5a826b472b
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-09-24 17:24:22 +0000

    js

--- Task_media_position_hover/app.js
+++ Task_media_position_hover/app.js
@@ -1,11 +1,26 @@
 "use strict";
 $(document).ready(function () {
-    $('input').click(function (e) {
+
+    // $('.btn').click(function (e) {
+    //     e.preventDefault();
+    //     $('.col-md-12').append(`<button></button>`);
+
+    //     $('input').val();
+    // });
+
+
+    $('.btn').click(function (e) {
         e.preventDefault();
-        $('h2').append('-necesen');
+
+        let a = (a) => {
+            for (let i = 1; i <= a; i++) {
+                $('.box').append(`<button>${i}</button>`);
+            }
+        }
+        a($('input').val())
     });
-
-
 });
+
+
 
 
