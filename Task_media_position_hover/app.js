5339a0ae65d12b9f04876eadd00ef1cb0f34506e
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-09-26 13:45:44 +0000

    js

--- Task_media_position_hover/app.js
+++ Task_media_position_hover/app.js
@@ -3,16 +3,16 @@ $(document).ready(function () {
 
     $('.btn').click(function (e) {
         e.preventDefault();
-        let text = $('input').val().trim();
-        if (text === '') return;
-        if (isNaN(text)) {
-            $('.col-md-12').append(`<button>${text}</button>`);
-        } else {
-            let a = parseInt(text);
-            for (let i = 1; i <= a; i++) {
-                $('.col-md-12').append(`<button>${i}</button>`);
+        let a = (m) => {
+            if (isNaN(m)) {
+                $('.col-md-12').append(`<button>${m}</button>`);
+            } else {
+                for (let i = 1; i <= m; i++) {
+                    $('.col-md-12').append(`<button>${i}</button>`);
+                }
             }
         }
+        a($('input').val());
         $('input').val('');
     });
 });
